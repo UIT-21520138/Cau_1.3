a1d4cb01eb8d7dcfc2815bb394a6e34affa6f32e
UIT.21520138 <21520138@gm.uit.edu.vn>
2024-06-20 18:01:20 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import random
+from PyDictionary import PyDictionary
+
+dictionary = PyDictionary()
+
+word_list = [
+    "aberration", "catalyst", "eccentric", "facetious", "garrulous",
+    "haphazard", "juxtapose", "luminous", "nefarious", "obfuscate",
+    "pensive", "quintessential", "recalcitrant", "serendipity", "tenacious",
+    "ubiquitous", "voracious", "winsome", "xenophobia", "zealous"
+]
+
+st.title("Random Words Generator")
+st.header("Random Word")
+
+if st.button("Generate"):
+    random_word = random.choice(word_list)
+    meaning = dictionary.meaning(random_word)
+    
+    st.header(f"{random_word.capitalize()}")
+    if meaning:
+        for key, value in meaning.items():
+            st.write(f"Meaning: ({key}) {', '.join(value)}")
+    else:
+        st.write("Meaning not found.")
